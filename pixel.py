f00b1f6e4288f5e8c90d6c7e1210a2de31f2cece
Gwonchan Jason Yoon <50343951+gwonchan@users.noreply.github.com>
2022-08-17 08:41:36 -0500

    Update pixel.py

--- pixel.py
+++ pixel.py
@@ -62,8 +62,6 @@ def send_text():
     el7.send_keys("+×÷=/_<>[]!@#$%^&*()-':;,?`~\|€£¥₩°•○●□■♤♡◇♧☆▪¤《》¡¿,.")
     el8.click()
 
-    el7.send_keys("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way - in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.")
-    el8.click()
 
 def send_picture():
     driver.implicitly_wait(wait_duration)
